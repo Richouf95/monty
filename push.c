f6891885bff30ec7fb6911c02e6a57733b61866e
Richouf95 <wourichouf95@gmail.com>
2023-12-14 16:37:02 -0800

    Update

--- push.c
+++ push.c
@@ -8,12 +8,6 @@
 
 void op_push(stack_t **stack, unsigned int line_number)
 {
-	/*if (data.x == 90500715)
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_number);
-		free_stack(stack);
-		exit(EXIT_FAILURE);
-	}*/
 	data.n = line_number;
 
 	addNode(stack, data.x);

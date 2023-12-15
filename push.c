84addd55d4c10c7864d1fc74bf59482e66ee5500
Richouf95 <wourichouf95@gmail.com>
2023-12-14 16:35:32 -0800

    Update

--- push.c
+++ push.c
@@ -8,11 +8,13 @@
 
 void op_push(stack_t **stack, unsigned int line_number)
 {
-	if (line_number == 90500715)
+	/*if (data.x == 90500715)
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		free_stack(stack);
 		exit(EXIT_FAILURE);
-	}
+	}*/
+	data.n = line_number;
 
 	addNode(stack, data.x);
 }
--- readMonty.c
+++ readMonty.c
@@ -10,6 +10,7 @@ void readMonty(char *fileName)
 	char line[25];
 	char op[25];
 	int i;
+	unsigned int n = 1;
 	instruction_t listFunctions[] = {
 		{"push", op_push}, {"pop", op_pop}, {"pall", op_pall},
 		{"pint", op_pint}, {NULL, NULL}
@@ -30,11 +31,19 @@ void readMonty(char *fileName)
 			{
 				if (strcmp(listFunctions[i].opcode, data.op) == 0)
 				{
-					listFunctions[i].f(data.stack, 0);
+					if (strcmp(data.op, "push") == 0 && data.x == 90500715)
+					{
+						fprintf(stderr, "L%d: usage: push integer\n", n);
+						free_stack(data.stack);
+						fclose(file);
+						exit(EXIT_FAILURE);
+					}
+					listFunctions[i].f(data.stack, n);
 					break;
 				}
 			}
 		}
+		n++;
 	}
 
 	fclose(file);
